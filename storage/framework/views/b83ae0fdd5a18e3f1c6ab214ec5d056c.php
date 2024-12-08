b199a149ca1ebd5bb354561516bd3c2b38bd6e2b
Mohamed Khater <mhmedkhaater@gmail.com>
2024-12-08 07:01:06 +0200

    update

--- storage/framework/views/b83ae0fdd5a18e3f1c6ab214ec5d056c.php
+++
@@ -1,9 +0,0 @@
- <!-- Spinner Start -->
- <div id="spinner"
-     class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-     <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
-         <span class="sr-only">Loading...</span>
-     </div>
- </div>
- <!-- Spinner End -->
-<?php /**PATH /home/mohamed-khater/Documents/projects/Digital-Livewire/DigitalLivewire/resources/views/front/layouts/partials/spinner.blade.php ENDPATH**/ ?>
